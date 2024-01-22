4e2924386f733d6d9bf8be21f56a7222a0b7baef
Luis Diaz <luisantoniodiaz97@gmail.com>
2024-01-21 21:02:04 -0600

    Corrections

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,9 +1,11 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import styles from '@/app/ui/home.module.css';
 import Link from 'next/link';
 
 export default function Page() {
   return (
+    // These are Tailwind classes
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
@@ -17,6 +19,7 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
+          <div className={styles.shape} />;
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"

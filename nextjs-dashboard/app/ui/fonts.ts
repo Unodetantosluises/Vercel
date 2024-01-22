30dc38c69a7eb434c853c025ff019946143db8a8
Luis Diaz <luisantoniodiaz97@gmail.com>
2024-01-22 06:40:58 -0600

    Progress

--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import { Inter, Montserrat } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin']});
-export const mont = Montserrat({ subsets: ['cyrillic']})
+export const mont = Montserrat({ weight: ['400', '700'], subsets: ['latin']})
